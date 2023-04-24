6b7fcf95388243ad0a620ea281a8a2f984246475
shrieghimire <shredde111@gmail.com>
2023-04-24 06:45:33 +0545

    front end probability fix 1

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,8 @@
 import DrawingBoard from "./Components/DrawingBoard/DrawingBoard";
-// import Image from "./Components/Image/Image";
-// import "./Components/Image/image.css";
 import ImageUpload from "./Components/ImageUpload/ImageUpload";
-
 function App() {
   return (
     <>
-      {/* <Image /> */}
       <DrawingBoard />
       <ImageUpload />
     </>
